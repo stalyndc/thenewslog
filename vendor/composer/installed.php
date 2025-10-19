4e6c916c28b88f1d2bd2557f461c17df5edbc192
Stalyn <stalyn@disla.net>
2025-10-19 13:59:09 -0400

    Initialize Composer dependencies and autoloading files
    - Created composer.lock to lock project dependencies.
    - Added vendor/autoload.php for autoloading classes.
    - Implemented vendor/composer/ClassLoader.php for PSR-0, PSR-4, and classmap loading.
    - Introduced vendor/composer/InstalledVersions.php to manage installed package versions.
    - Added LICENSE file for Composer licensing.
    - Generated autoload_classmap.php, autoload_namespaces.php, autoload_psr4.php, autoload_real.php, and autoload_static.php for autoloading functionality.
    - Created installed.json and installed.php to track installed packages and their versions.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'thenewslog/thenewslog',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '35148a08adeff7e41b6e46261eb6f32519464cee',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'thenewslog/thenewslog' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '35148a08adeff7e41b6e46261eb6f32519464cee',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
