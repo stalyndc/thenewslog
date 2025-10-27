276f13570d8560c6ae9a34d141152fded85bbd5a
Stalyn <stalyn@disla.net>
2025-10-27 07:59:38 -0400

    fix: update asset versioning and enhance build configuration
    - Updated asset versioning for `app.css` and `app.js` to version 17 to ensure the latest styles and scripts are loaded.
    - Modified `vite.config.ts` to disable CSS code splitting and set consistent output file names for better asset management.

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,20 @@ export default defineConfig({
   build: {
     outDir: "../assets",
     emptyOutDir: false,
+    cssCodeSplit: false,
     rollupOptions: {
       input: {
         app: "resources/ts/app.ts"
+      },
+      output: {
+        entryFileNames: "app.js",
+        assetFileNames: (assetInfo) => {
+          const name = assetInfo.name ?? "asset";
+          if (name.endsWith('.css')) {
+            return 'app.css';
+          }
+          return "[name][extname]";
+        }
       }
     }
   }
